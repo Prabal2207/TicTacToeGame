71695d808e7c55179d6330db620b4dbc322e2de6
Prabal2207 <prabal760@gmail.com>
2021-07-21 13:00:06 +0530

    Added board game

---
+++ Tictactoe1/Tictactoe1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tictactoe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to tictactoe!");
+            TicTacToeGame obj = new TicTacToeGame();
+            obj.tictactoe();
+            //obj.ChooseXorO();
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Tictactoe1/Tictactoe1/TicTacToeGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Tictactoe1
+{
+    public class TicTacToeGame
+    {
+        int player = 1;
+       public void tictactoe()
+        {
+            char[] board = new char[10];
+            for(int i=1;i<10;i++)
+            {
+                board[i] = '_';
+            }
+        }
+        
+    }
+}
